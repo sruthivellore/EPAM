3b16a15e6fe1f71997a75c9d973ce304d471b0c6
sruthivellore <35759709+sruthivellore@users.noreply.github.com>
2019-04-13 15:16:25 +0530

    Task4

---
+++ Epam_Task4/Huffman.java
@@ -0,0 +1,16 @@
+void decode_huff(node * root, string s)
+{
+    char *str = strcpy((char *)calloc(1 << 20, sizeof(char)), s.c_str());
+    node *r = root;
+    for (int i=0; i<=strlen(str); i++) {
+        if (r->data > 0) {
+            cout << r->data;
+            r = root;
+        }
+        
+        if (str[i] == '0')
+            r = r->left;
+        else
+            r = r->right;
+    }
+}
